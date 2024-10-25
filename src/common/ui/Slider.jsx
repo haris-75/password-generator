6568a1ac82912356ac6e1129f6de0ed5263ea50d
haris-75 <bitworld75@gmail.com>
2024-10-25 16:04:12 +0500

    Add Password Slider

--- src/common/ui/Slider.jsx
+++ src/common/ui/Slider.jsx
@@ -1,6 +1,36 @@
 
-export function Slider() {
+export const Slider = ({
+    min = 0,
+    max = 50,
+    id,
+    value,
+    onChange,
+    name,
+    className = '',
+}) => {
     return (
-        <div>Slider</div>
-    )
-}
+        <div className={className}>
+            <input
+                type="range"
+                id={id}
+                name={name}
+                min={min}
+                max={max}
+                className="w-full h-[2px] bg-secondary appearance-none cursor-pointer accent-primary
+                [&::-webkit-slider-thumb]:appearance-none 
+                [&::-webkit-slider-thumb]:h-5 
+                [&::-webkit-slider-thumb]:w-5
+                [&::-webkit-slider-thumb]:bg-primary
+                [&::-webkit-slider-thumb]:border-2
+                [&::-webkit-slider-thumb]:hover:bg-warning
+                [&::-moz-range-thumb]:h-5 
+                [&::-moz-range-thumb]:w-5
+                [&::-moz-range-thumb]:bg-primary 
+                [&::-moz-range-thumb]:border-2
+                [&::-moz-range-thumb]:hover:bg-warning"
+                onChange={onChange}
+                value={value}
+            />
+        </div>
+    );
+};
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,17 +1,26 @@
 import { CHECKBOX_LIST } from '@constants';
 import { ArrowIcon } from '@svg';
-import { Button, Checkbox, Input } from '@ui';
-import { useState } from 'react';
+import { Button, Checkbox, Input, Slider } from '@ui';
+import { useEffect, useState } from 'react';
 
 export function PasswordGenerator() {
     const [checkboxObj, setCheckboxObj] = useState({ ...CHECKBOX_LIST });
     const [password, setPassword] = useState('');
     const onChangeHandler = (e, id) => setCheckboxObj({ ...checkboxObj, [id]: { value: e.target.checked, label: checkboxObj?.[id]?.label } });
-
+    const [sliderValue, setSliderValue] = useState(5);
+    useEffect(() => {
+        console.log('onChangeHandler', sliderValue)
+    }, [sliderValue])
     return (
         <div>
             <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
             <Button variant='primary'> Copy Password_ <ArrowIcon color='black' /></Button>
+            <Slider
+                value={sliderValue}
+                onChange={(e) => setSliderValue(e.target.value)}
+                id="password-slider"
+                name="password-slider"
+            />
             <div className='grid grid-cols-2 grid-row-2'>
                 {Object.keys(checkboxObj)?.map((id) =>
                     <Checkbox
